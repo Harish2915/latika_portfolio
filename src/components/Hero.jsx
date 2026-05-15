13f8424e26bfe83b8ec843ba410bb0544155b6a0
Harish2915 <harish29012004@gmail.com>
2026-05-15 12:52:52 +0530

    feat: update Hero component to replace icons with new ones

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -89,16 +89,16 @@ function Hero() {
 
             {/* Floating Icons */}
 
-            {/* <div className="floating-icon icon1">
-              <FaCode />
-            </div> */}
-
             <div className="floating-icon icon1">
-              <FaReact />
+              <FaCode />
             </div>
 
             <div className="floating-icon icon2">
               <FaFlutter />
+            </div>
+
+            <div className="floating-icon icon3">
+              <FaReact />
             </div>
 
           </div>
