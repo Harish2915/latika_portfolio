6c16067ea39c4409b1f2f2cee4878e3381b18147
Harish2915 <harish29012004@gmail.com>
2026-05-18 18:28:13 +0530

    y

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -46,7 +46,7 @@ function Hero() {
                 Contact Me
                 <FaArrowRight />
               </a>
-              <a href="../../public/resume/Latika_Resume.pdf" 
+              <a href="/resume/Latika_Resume.pdf" 
               target="_blank" 
               rel="noopener noreferrer"
               download
